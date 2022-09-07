4e1a0fc69dbcf986e316d9e8843856fcda50559e
Jhonata Peres <jhonatapers@gmail.com>
2022-09-07 15:22:22 -0300

    Classe AlgoritmoAEstrela Adicionado

---
+++ src/main/java/com/jhonatapers/labirinto/serivce/impl/AlgorimoAEstrela.java
@@ -0,0 +1,18 @@
+package com.jhonatapers.labirinto.serivce.impl;
+
+import com.jhonatapers.labirinto.model.GeneVo;
+import com.jhonatapers.labirinto.serivce.IAlgoritmo;
+
+public class AlgorimoAEstrela implements IAlgoritmo {
+
+    @Override
+    public GeneVo inicia() {
+        return null;
+    }
+
+    @Override
+    public IAlgoritmo debug(boolean ativo) {
+        return null;
+    }
+    
+}
